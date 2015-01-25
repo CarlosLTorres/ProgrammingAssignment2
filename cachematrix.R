1469b3e5cafe1a0c3f5e1903db37b11b1460fda6
CarlosLTorres <carlost@unb.br>
2015-01-25 21:40:19 -0200

    My work to the Assignment 2 of the R Programming of Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The two functions below, used in sequence, are designd to calculate the inverse of a matrix x. 
+# If there is a previous inverse matrix in cache it is not necessary recalculation.
 
-## Write a short comment describing this function
+# (1) The makeCacheMatrix function of x = matrix():
 
-makeCacheMatrix <- function(x = matrix()) {
+#   A variable 'm' to save inverse matrix in cache;
+#   A function get() to obtain "raw" matrix (to find its inverse);
+#   A function setImatrix() to assign the inverse matrix (of x) to m;
+#   A function getImatrix() to obtain the cached inverse matrix.
 
+makeCacheMatrix <- function(x=matrix()) {
+    m <- NULL
+    get <- function() x
+    setImatrix <- function(Imatrix) m <<- Imatrix
+    getImatrix <- function() m    
+    list(get=get, setImatrix=setImatrix, getImatrix=getImatrix) # return a list of functions as an R object
 }
 
+# (2) The cacheSolve function to invert the matrix x
 
-## Write a short comment describing this function
+#   It checks if the is a inverse matrix in cache;
+#   If yes, returns the result and quits:
+#   If not, the inverse of x is calculated, saved to cached, and returned.
+#   It is returned a list calling makeCacheMatrix(x).
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getImatrix()
+    if(!is.null(m)) {
+        message("Inverse Matrix was found. Done.")
+        return(m)
+    } else {
+        message("No cached data was found. Calcule inverse matrix")
+        data <- x$get() # obtains matrix from object x
+        m <- solve(data) # finds inverse matrix
+        x$setImatrix(m) # assigns resulting inverse matrix to object x
+        message("Done.")
+        return(m)
+    }
 }
